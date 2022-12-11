2aa252350322012e96145affb5775a94e49e4f1a
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-11 20:20:45 +0100

    adds get playlists endpoint

--- server.js
+++ server.js
@@ -1,40 +1,48 @@
-'use strict';
+"use strict";
 
-const mysql = require('mysql');
-const express = require('express');
+const mysql = require("mysql");
+const express = require("express");
 const app = express();
 const port = 3000;
-const mm = require('musicmetadata');
-const fs = require('fs');
-const audioFolder = 'public/audio';
+const mm = require("musicmetadata");
+const fs = require("fs");
+const audioFolder = "public/audio";
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'music_player',
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "music_player",
 });
 
 conn.connect((err) => {
-    if(err) {
-		console.log(err, `The database connection couldn't be established`);
-		return;
-	} else {
-		console.log(`Connection established`);
-	}
+  if (err) {
+    console.log(err, `The database connection couldn't be established`);
+    return;
+  } else {
+    console.log(`Connection established`);
+  }
 });
 
 // GET renders a static HTML
 
-app.get('/player', (req, res) => {
-    res.sendFile(__dirname + '/public/main.html');
+app.get("/player", (req, res) => {
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+// GET /playlists
+
+app.get("/playlists", (req, res) => {
+  queryDb("SELECT * FROM playlists")
+    .then((data) => res.status(200).json(data))
+    .catch((err) => console.log(err));
 });
 
 // PORT LISTEN
 
 app.listen(port, () => {
-    console.log(`The server is up and running on ${port}`);
+  console.log(`The server is up and running on ${port}`);
 });
