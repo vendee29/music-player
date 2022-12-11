9a188c2f5d8abf6796ce50ab3b8b6dec5164f3f6
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-11 20:23:21 +0100

    adds post playlists endpoint

--- server.js
+++ server.js
@@ -41,8 +41,60 @@ app.get("/playlists", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+// POST /playlists // title is required
+
+app.post('/playlists', (req, res) => {
+    let title = req.body.title;
+
+    if(title == 'null' || title == null || title == undefined) {
+        res.status(400).json({
+            message: 'No playlist was added.'
+        });
+    }
+
+    createPlaylist(title)
+    .then(result => {
+        if(result.message == 'This title is already in use.') {
+            res.status(400).json(result);
+        } else {
+            res.status(200).json(result);
+        }
+    })
+    .catch(err => console.log(err))
+})
+
 // PORT LISTEN
 
 app.listen(port, () => {
   console.log(`The server is up and running on ${port}`);
 });
+
+// HELPER FUNCTIONS
+
+function queryDb(sqlQuery, valuesArr) {
+    return new Promise((resolve, reject) => {
+        conn.query(sqlQuery, valuesArr, (err, result) => {
+            if(err) {
+                return reject('DATABASE ERROR');
+            } else {
+                return resolve(result);
+            }
+        })
+    }) 
+}
+
+// create playlist
+
+async function createPlaylist(title) {
+    let select = await queryDb('SELECT * FROM playlists WHERE title = ?', [title]);
+    if(select.length > 0) {
+        return {
+            'message': 'This title is already in use.'
+        }
+    } else {
+        let insert = await queryDb('INSERT INTO playlists (title) VALUES (?)', [title]);
+        return {
+            'message': `The playlist ${title} was added.`
+        }
+    }
+}
