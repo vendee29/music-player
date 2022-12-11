b70bb4b111f76fd7833726f5737eb5d2102274e5
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-11 20:46:55 +0100

    adds play and pause logic

--- public/main.js
+++ public/main.js
@@ -34,3 +34,96 @@ const endTime = document.querySelector(".end-time");
 const audio = document.querySelector("audio");
 
 localStorage.setItem("index", 0);
+
+// PLAY BUTTON
+
+playBtn.addEventListener("click", () => {
+    let isPlaying = controlPanel.classList.contains("is-playing");
+  
+    if (!isPlaying) {
+      playTrack();
+    } else {
+      pauseTrack();
+    }
+  });
+
+// HELPER FUNCTIONS
+
+function loadTrack(track_id) {
+    localStorage.setItem("track_id", track_id);
+    const tracksTableRows = document.querySelectorAll(".tracks-table tr");
+    for (let i = 0; i < tracksTableRows.length; i++) {
+      if (tracksTableRows[i].getAttribute("track_id") == track_id) {
+        title.textContent = tracksTableRows[i].getAttribute("track_title");
+        artist.textContent = tracksTableRows[i].getAttribute("track_artist");
+        endTime.innerHTML = tracksTableRows[i].getAttribute("track_duration");
+        audio.src = tracksTableRows[i].getAttribute("track_path");
+      }
+    }
+    progressBar.value = 0;
+    albumCover.setAttribute(
+      "style",
+      `background: url('img/${track_id}.jpg'); background-size: contain; background-repeat: no-repeat; background-position: center;`
+    );
+  
+    setAsFavorite(track_id)
+      .then((result) => {
+        playTrack();
+      })
+      .catch((err) => console.log(err));
+  }
+  
+  async function setAsFavorite(track_id) {
+    let response = await fetch(`/playlist-tracks/1/${track_id}`);
+    let isFavorite = await response.json();
+    if (isFavorite) {
+      addSongToFavorites.style.filter = "brightness(0)";
+    } else {
+      addSongToFavorites.style.filter = "brightness(1)";
+    }
+    return isFavorite;
+  }
+  
+  function playTrack() {
+    playBtn.classList.add("pause");
+    playBtn.classList.remove("play");
+    controlPanel.classList.add("is-playing");
+    audio.play();
+  }
+  
+  function pauseTrack() {
+    playBtn.classList.add("play");
+    playBtn.classList.remove("pause");
+    controlPanel.classList.remove("is-playing");
+    audio.pause();
+  }
+  
+  function clearNotifications() {
+    notifications.innerHTML = "";
+  }
+  
+  function getTime(seconds) {
+    let s = parseInt(seconds % 60);
+    let m = parseInt((seconds / 60) % 60);
+    let mm = m < 10 ? "0" + m : m;
+    let ss = s < 10 ? "0" + s : s;
+    return mm + ":" + ss;
+  }
+  
+  function setSelectedRow(row) {
+    if (row.classList.contains("not-selected")) {
+      row.classList.remove("not-selected");
+      row.classList.add("selected");
+    }
+  }
+  
+  function setNotSelectedRow(tableClass) {
+    let rows = document.querySelectorAll(`.${tableClass} tr`);
+    for (let i = 0; i < rows.length; i++) {
+      if (rows[i].classList.contains("selected")) {
+        rows[i].classList.remove("selected");
+        rows[i].classList.add("not-selected");
+      }
+    }
+  }
+  
